7989614e5cc6489e7af573a51d3e30b77d6ae164
Rosano Coutinho <pub@xeari.com>
2021-03-08 12:06:58 -0300

    Extract OLSKObjectJSONSchema

--- main-tests.js
+++ main-tests.js
@@ -1,4 +1,4 @@
-const { deepEqual } = require('assert');
+const { deepEqual, throws } = require('assert');
 
 const mod = require('./main.js').default;
 
@@ -19,6 +19,153 @@ describe('OLSKObjectSafeCopy', function test_OLSKObjectSafeCopy() {
 	
 	it('ignores $dynamic fields', function () {
 		deepEqual(mod.OLSKObjectSafeCopy(item).$charlie, undefined);
+	});
+
+});
+
+describe('_OLSKObjectInferredType', function test_OLSKObjectInferredType() {
+
+	it('throws error if not string', function() {
+		throws(function() {
+			mod._OLSKObjectInferredType(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('converts if string', function() {
+		deepEqual(mod._OLSKObjectInferredType('XYZErrorNotString'), 'string');
+	});
+
+	it('converts if boolean', function() {
+		deepEqual(mod._OLSKObjectInferredType('XYZErrorNotBoolean'), 'boolean');
+	});
+
+	it('converts if date', function() {
+		deepEqual(mod._OLSKObjectInferredType('XYZErrorNotDate'), 'date');
+	});
+
+	it('converts variable prefix', function() {
+		deepEqual(mod._OLSKObjectInferredType('ABCErrorNotDate'), 'date');
+	});
+
+});
+
+describe('OLSKObjectJSONSchema', function OLSKObjectJSONSchema() {
+
+	it('throws error if not object', function() {
+		throws(function() {
+			mod.OLSKObjectJSONSchema(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns object', function() {
+		deepEqual(mod.OLSKObjectJSONSchema({}), {
+			type: 'object',
+			properties: {},
+			required: [],
+		});
+	});
+
+	context('properties', function() {
+		
+		it('declares string', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotString']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares boolean', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotBoolean']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'boolean',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares date', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotDate']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+						format: 'date-time',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares filled', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotFilled']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+	});
+
+	context('required', function() {
+		
+		it('declares if required', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotString']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+
+		it('ignores', function() {
+			deepEqual(mod.OLSKObjectJSONSchema({
+				alfa: ['XYZErrorNotString', '__RSOptional']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [],
+			});
+		});
+		
 	});
 
 });
--- main.js
+++ main.js
@@ -10,6 +10,43 @@ const mod = {
 		}, {});
 	},
 
+	_OLSKObjectInferredType (inputData) {
+		if (typeof inputData !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return inputData.replace(/\w+ErrorNot/, '').toLowerCase();
+	},
+
+	OLSKObjectJSONSchema (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return {
+			type: 'object',
+			properties: Object.entries(inputData).reduce(function (coll, [key, val]) {
+				coll[key] = {};
+
+				coll[key].type = mod._OLSKObjectInferredType([...val].shift()).replace('filled', 'string');
+
+				if (coll[key].type === 'date') {
+					coll[key].type = 'string';
+					coll[key].format = 'date-time';
+				}
+
+				return coll;
+			}, {}),
+			required: Object.entries(inputData).filter(function ([key, val]) {
+				return !val.includes('__RSOptional');
+			}).map(function ([key, val]) {
+				return key;
+			}),
+		};
+	},
+
+	
+
 };
 
 export default mod;
